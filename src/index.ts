6c70a1faaecec43528479d29c1ba2c70089031a6
Ray Kim <yomandawgshit@gmail.com>
2020-08-13 19:15:37 +0900

    auth setup with cookie-session

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['asdfaf'] }));
+app.use(router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send(`
